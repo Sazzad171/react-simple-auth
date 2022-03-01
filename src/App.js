81b4fcb2e1bc25e231e5c43d5ce781ff86a2da8b
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-03-01 12:49:13 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+// for router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// axios
+import axios from "axios";
+
+// css import
+import "./assets/css/style.css"
+
+// all components import
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Test code</h1>
+    </div>
+  );
+}
+
+export default App;
